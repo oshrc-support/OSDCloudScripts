ff6530db980b4b565f210b61108419cd6145280e
Fayese <support@oshrc.gov>
2024-02-14 21:33:47 -0500

    commit

--- Azure Function/tempCodeRunnerFile.ps1
+++ Azure Function/tempCodeRunnerFile.ps1
@@ -1,5 +1,3 @@
-﻿az account set --subscription 4dc12530-2664-4d5a-853b-c32a1c90b2da
-
 az group create --name oshrc-prod-osdcloud-rg --location eastus
 
 az deployment group create --resource-group oshrc-prod-osdcloud-rg --template-file deployment.bicep
